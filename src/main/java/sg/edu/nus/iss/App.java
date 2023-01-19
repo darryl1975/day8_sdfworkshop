205f21b4f285eb4a11c271339b7c3e06ea3e7da1
Darryl Ng <darryl1975@hotmail.com>
2023-01-19 12:08:23 +0800

    ConcurrentHashMapExample

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -14,7 +14,10 @@ public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        CollectionSortExample cse = new CollectionSortExample();
-        cse.example02();
+        //CollectionSortExample cse = new CollectionSortExample();
+        //cse.example02();
+
+        ConcurrentHashMapExample chme = new ConcurrentHashMapExample();
+        chme.example();
     }
 }
